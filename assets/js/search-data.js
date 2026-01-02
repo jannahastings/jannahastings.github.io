9dd3ece38b46aa9b29be354b97a33513313a1fa6
jannahastings <janna.hastings@gmail.com>
2026-01-02 10:43:43 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@01712ab9b6baea04468d1c1f6430beba91cf3137 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,13 +489,6 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
@@ -507,14 +500,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=cz-hhPUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
