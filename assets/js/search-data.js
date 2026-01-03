1e7fabe21219aa7f917ca4f6de4068f039a20887
jannahastings <janna.hastings@gmail.com>
2026-01-03 12:13:26 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@5ab42717f2fb598dd414f2b120f4553439730ae3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -392,11 +392,11 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-ai-for-scientific-discovery",
+          title: 'AI for scientific discovery',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/ai4sd/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
