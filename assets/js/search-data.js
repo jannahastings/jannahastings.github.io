57730fb0539bce17d17cc0ff1d41f46d46638ed8
jannahastings <janna.hastings@gmail.com>
2026-01-05 15:02:44 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@2a599e5b00dcaacdb96c47ab0a0d56fae6edeba3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,25 +454,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%6A%61%6E%6E%61.%68%61%73%74%69%6E%67%73@%69%64%69%61%70.%63%68", "_blank");
         },
       },{
         id: 'social-scholar',
