7ed249a2da654e65435bb42041f1d498c5dd2c92
jannahastings <janna.hastings@gmail.com>
2026-01-05 15:26:52 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@8875902d2d79767653b3e4e652fd15e38c74a93a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,18 +397,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/ai4sd/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-the-mkds-group-moves-to-idiap-and-becomes-the-hchai-group",
+          title: 'The MKDS group moves to Idiap, and becomes the HCHAI group!',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
