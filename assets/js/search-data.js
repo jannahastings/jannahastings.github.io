cd8bb304cee0ae6ff837cbd46e098a5b9278e22f
jannahastings <janna.hastings@gmail.com>
2026-01-09 17:08:55 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@5e8bca1a57c3968772088a73e63d724e665a64b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,51 +402,31 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-galenos",
+          title: 'GALENOS',
+          description: "A new living evidence resource of early phase research for research prioritisation in mental health",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-strontex",
+          title: 'StrOntEx',
+          description: "Algorithms for ontology extension and ontology-based knowledge injection for small molecule metabolites",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-metabolinkai",
+          title: 'MetaboLinkAI',
+          description: "Transforming metabolomics data into actionable insights through AI-powered, knowledge graph-driven solutions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-unrealbody",
+          title: 'UnRealBody',
+          description: "Inoculating Young People Against Harm from Unrealistic AI-Based Body Images",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-apricot",
+          title: 'APRICOT',
+          description: "...",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
