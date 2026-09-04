609a6c4ef613e259d95df35602d7ddb1c103b3ee
schnamo <18518966+schnamo@users.noreply.github.com>
2026-09-04 12:48:58 +0000

    Deploying to gh-pages from @ jannahastings/jannahastings.github.io@ac429387c8d0b585e3fd2da22e014794906de245 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,6 +427,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_20260701/";
+            },},{id: "news-join-us-for-the-idiap-open-day-on-the-7th-of-september-2026",
+          title: 'Join us for the IDIAP open day on the 7th of September 2026!...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_20260904/";
             },},{id: "projects-galenos",
           title: 'GALENOS',
           description: "A new living evidence resource of early phase research for research prioritisation in mental health",
